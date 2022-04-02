34a3734b7000d8dc460d034edcb6a57a667d43ea
thaisealves <alves.thaisesilva@gmail.com>
2022-04-02 03:44:54 -0300

    Selecionando separadamente

--- scripts.js
+++ scripts.js
@@ -1,11 +1,54 @@
-function foodSelected(clicked){
+let food;
+let drink;
+let dessert;
 
-    const boxSelected = document.querySelector(".item-selected");
+function foodSelected(element){
+
+    food = element.innerText;
+
+    const boxSelected = document.querySelector(".food").querySelector(".item-selected");
     if (boxSelected!==null){
         boxSelected.classList.remove("item-selected")
         boxSelected.classList.add("item-box")
     }
 
-    clicked.classList.remove("item-box")
-    clicked.classList.add("item-selected")
+    element.classList.remove("item-box")
+    element.classList.add("item-selected")
+}
+
+function drinkSelected(element){
+
+    drink = element.innerText;
+
+    const boxSelected = document.querySelector(".drink").querySelector(".item-selected");
+    if (boxSelected!==null){
+        boxSelected.classList.remove("item-selected")
+        boxSelected.classList.add("item-box")
+    }
+
+    element.classList.remove("item-box")
+    element.classList.add("item-selected")
+}
+
+function dessertSelected(element){
+     dessert = element.innerText;
+    const boxSelected = document.querySelector(".dessert").querySelector(".item-selected");
+    if (boxSelected!==null){
+        boxSelected.classList.remove("item-selected")
+        boxSelected.classList.add("item-box")
+    }
+
+    element.classList.remove("item-box")
+    element.classList.add("item-selected")
+}
+
+function finalCheck(element){
+    
+      const boxSelected =  document.querySelector(".bottom").querySelector("button");
+
+      element.classList.add("checking");
+    
+
+
+
 }
