5a8039cd657d7bf7f4466f2c9aa06168ecb60cf8
thaisealves <alves.thaisesilva@gmail.com>
2022-04-04 13:05:19 -0300

    Projeto completamente finalizado

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 
 let allItems = 0;
+let url; 
+let restaurantNumber = 5524981160258;
+let nameFood, nameDrink, nameDessert;
 
 function foodSelected(element){
     let boxSelected = document.querySelector(".food").querySelector(".item-selected");
@@ -60,9 +63,9 @@ function finalCheck(){
     let drinkSelected = document.querySelector(".drink").querySelector(".item-selected").querySelector("h6").innerHTML;
     let dessertSelected = document.querySelector(".dessert").querySelector(".item-selected").querySelector("h6").innerHTML;
 
-    let nameFood = document.querySelector(".food").querySelector(".item-selected").querySelector("h3").innerHTML;
-    let nameDrink = document.querySelector(".drink").querySelector(".item-selected").querySelector("h3").innerHTML;
-    let nameDessert = document.querySelector(".dessert").querySelector(".item-selected").querySelector("h3").innerHTML;
+     nameFood = document.querySelector(".food").querySelector(".item-selected").querySelector("h3").innerHTML;
+     nameDrink = document.querySelector(".drink").querySelector(".item-selected").querySelector("h3").innerHTML;
+     nameDessert = document.querySelector(".dessert").querySelector(".item-selected").querySelector("h3").innerHTML;
     
     //space to tranform the price in a NUMVER
     let priceFood = Number(foodSelected.replace(",", ".").replace("R$", ""));
@@ -72,7 +75,7 @@ function finalCheck(){
     let fullPrice = priceFood + priceDrink + priceDessert;
 
     
-    console.log(fullPrice)
+    
 
     document.querySelector(".confirm").querySelector(".value1").innerHTML = foodSelected.replace("R$", "");
     document.querySelector(".confirm").querySelector(".value2").innerHTML = drinkSelected.replace("R$", "");
@@ -83,15 +86,24 @@ function finalCheck(){
     document.querySelector(".confirm").querySelector(".item3").innerHTML = nameDessert;
 
     document.querySelector(".confirm").querySelector(".valueTotal").innerHTML = `R$ ${(fullPrice.toFixed(2).toString()).replace(".",",")}`;
-   
+
+    
+    let name = prompt("Qual seu nome?")
+    let adress = prompt("E o endereço?")
+    let textMessage = `Olá, gostaria de fazer o pedido: \n- Prato: ${nameFood}\n- Bebida: ${nameDrink}\n- Sobremesa: ${nameDessert}\nTotal: R$ ${fullPrice.toFixed(2)}
+    Nome: ${name}\nEndereço: ${adress}`
+       let params = encodeURIComponent(textMessage);
+    
+        url = `https://wa.me/${restaurantNumber}?text=${params}`;
+
 }
 
 function cancel(){
     document.querySelector(".confirm").classList.add("hidden")
 }
 
-function replacing (){
-
+function ordering (){
+   window.location.assign(url);
 
 }
 
